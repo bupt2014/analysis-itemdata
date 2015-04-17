288851800656beec905c6e532efb30d01b14aaf0
2015_hadoop_houjincheng <houjincheng1992@163.com>
2015-04-17 15:50:29 +0800

    add import csv item file

---
+++ import_csv_item.py
@@ -0,0 +1,22 @@
+#import data from csv to mysql
+#import tianchi data train_user data into mysql
+#just for tianchi_mobile_recommend_train_user
+import csv
+import MySQLdb
+
+mydb = MySQLdb.connect(host='127.0.0.1',
+    user='root',
+    passwd='root',
+    db='tianchi')
+cursor = mydb.cursor()
+
+fd=open('tianchi_mobile_recommend_train_item.csv','r')
+fd.readline()
+csv_data = csv.reader(fd)
+cursor.executemany('INSERT INTO tianchi_mobile_recommend_train_user( item_id, item_geohash, item_category)' \
+          'VALUES(%s, %s, %s)',
+          csv_data)
+#close the connection to the database.
+mydb.commit()
+cursor.close()
+print "Done"
